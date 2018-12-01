544e9819d68d94fc82ecfe0c6b89061f49f5d14d
Beshlei <beshleytaras@gmail.com>
2018-12-01 20:06:32 +0200

    salary_fix

--- bosses/bosses.py
+++ bosses/bosses.py
@@ -1,4 +1,10 @@
-
+sorted_graph = dict()
+time_graph = dict()
+workers_salary = dict()
+some_set = set()
+time_dict = dict()
+list_for_keys = []
+salary_list = []
 
 def data_from_file(name_of_file):
     file = open(name_of_file, 'r')
@@ -32,6 +38,7 @@ def graph(vertex_list, subordinates):
     graph = dict()
     for i in range(len(vertex_list)):
         graph[vertex_list[i]] = subordinates[i]
+
     return graph
 
 def select_menegers(employees_list):
@@ -42,19 +49,39 @@ def select_menegers(employees_list):
             for subordinates_emloyeer in employees_list[i]:
                 print(str(i) + " manager for " + str(subordinates_emloyeer))
 
-def salary(employees_list):
-    salary_list = []
-    for i in range(len(employees_list)):
-        if not employees_list[i]:
-            one = 1
-            print("Salary" + "(" + str(i) + ") =" + str(one))
-            salary_list.append(one)
-        else:
-            subordinate_salary_count = 0
-            for subordinate_salary_count in range(len(employees_list[i])):
-                subordinate_salary_count += 1
 
-            print("Salary" + "(" + str(i) + ") =" + str(subordinate_salary_count))
-            salary_list.append(subordinate_salary_count)
+def salary(graph):
+    sorting_dict(graph)
+    print(sorted_graph)
+    print(sum((int(sorted_graph[j]) for j in range(0, int(len(sorted_graph))))))
+
+
+
+def sorting_dict(graph):
+    for i in graph.keys():
+        time_graph[i] = len(graph[i])
+        some_set.add(len(graph[i]))
+    for j in some_set:
+        for k in graph.keys():
+            if (len(graph[k]) == j):
+                sorted_graph[k] = graph[k]
+    return sorted_graph
+
+def salary(sorted_graph):
+    for n in sorted_graph.keys():
+        if (len(sorted_graph[n]) == 0):
+            workers_salary[n] = 1
+        for m in sorted_graph.keys():
+            for b in sorted_graph[m]:
+                if b == n:
+                    time_dict[m] = workers_salary[n]
+                    if m in workers_salary.keys():
+                        workers_salary[m] = time_dict[m] + workers_salary[m]
+                    else:
+                        workers_salary.update(time_dict)
+                    time_dict.clear()
+                    
+    for b in workers_salary.keys():
+            salary_list.append(workers_salary[b])
+
     print(sum((int(salary_list[j]) for j in range(0, int(len(salary_list))))))
-
--- bosses/main.py
+++ bosses/main.py
@@ -1,4 +1,4 @@
-from bosses import data_from_file, index_of_subordinates, graph, select_menegers, salary, list_of_vertex
+from bosses import data_from_file, index_of_subordinates, graph, select_menegers, list_of_vertex, sorting_dict, salary
 
 if __name__ == '__main__':
     data_of_matrix = data_from_file("in")
@@ -6,4 +6,6 @@ if __name__ == '__main__':
     index = index_of_subordinates(data_of_matrix)
     employeers = graph(list_of_vertex1, index)
     select_menegers(employeers)
-    salary(employeers)
+    sorting_dict(employeers)
+    salary(sorting_dict(employeers))
+
